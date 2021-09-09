0b7b3d07a6d6919e6918454fc0b36905fa5a8678
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2021-09-09 13:49:58 -0500

    Models and Fomrs.

---
+++ Api_Maps/users/forms.py
@@ -0,0 +1,94 @@
+"""Users forms."""
+
+# Django
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import fields
+from django.forms import ModelForm
+
+# Models
+from .models import Profile
+
+
+class UserForm(UserCreationForm):
+    """Form that uses to handle user creation."""
+
+    first_name = forms.CharField(
+        max_length=30, required=True, 
+        widget=forms.TextInput(attrs={'placeholder': '*Your first name.'}))
+    
+    last_name = forms.CharField(
+        max_length=30, required=True,
+        widget=forms.TextInput(attrs={'placeholder': '*Your last name.'}))
+
+    username = forms.EmailField(
+        max_length=255, required=True, 
+        widget=forms.TextInput(attrs={'placeholder': '*Your email.'}))
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '*Password.'}))
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '*Confirm Password.'}))
+
+    # reCaptcha token
+    token = forms.CharField(widget=forms.HiddenInput())
+
+    class Meta:
+        """Meta options."""
+        model = User
+        fields = (
+            'first_name', 'last_name', 
+            'username', 'password1', 
+            'password2')
+
+
+class AuthForm(AuthenticationForm):
+    """Form that uses to handle user auth."""
+
+    username = forms.EmailField(
+        max_length=255, required=True, 
+        widget=forms.TextInput(attrs={'placeholder': '*Email.'}))
+    
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '*Password.'}))
+
+    class Meta:
+        """Meta options."""
+        model = User
+        fields = ('username', 'password')
+
+
+class ProfileForm(forms.ModelForm):
+    """Profile form."""
+
+    address = forms.CharField(
+        max_length=100, required=True, widget=forms.HiddenInput())
+    
+    town = forms.CharField(
+        max_length=100, required=True, widget=forms.HiddenInput())
+    
+    county = forms.CharField(
+        max_length=100, required=True, widget=forms.HiddenInput())
+
+    country = forms.CharField(
+        max_length=60, required=True, widget=forms.HiddenInput())
+
+    post_code = forms.CharField(
+        max_length=8, required=True, widget=forms.HiddenInput())
+    
+    longitude = forms.CharField(
+        max_length=50, required=True, widget=forms.HiddenInput())
+
+    latitude = forms.CharField(
+        max_length=50, required=True, widget=forms.HiddenInput())
+
+    class Meta:
+        """Meta options."""
+        model = Profile
+        fields = (
+            'address', 'town', 
+            'country', 'post_code', 
+            'longitude', 'latitude')
--- Api_Maps/users/models.py
+++ Api_Maps/users/models.py
@@ -1,3 +1,30 @@
+"""Users models."""
+
+# Django
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Profile model.
+    Model One to One with User model.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    address = models.CharField(max_length=100, null=True)
+    town = models.CharField(max_length=100, null=True)
+    county = models.CharField(max_length=100, null=True)
+    country = models.CharField(max_length=60, null=True)
+    post_code = models.CharField(max_length=100, null=True)
+
+    longitude = models.CharField(max_length=50, null=True)
+    latitude = models.CharField(max_length=50, null=True)
+
+    captcha_score = models.FloatField(default=0.0)
+    has_profile = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.user}'
