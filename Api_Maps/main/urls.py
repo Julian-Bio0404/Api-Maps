db6b61a38ed147625cf54224f61d35413c42b26c
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2021-09-11 12:35:46 -0500

    URLs and Users signals.

--- Api_Maps/main/urls.py
+++ Api_Maps/main/urls.py
@@ -1,6 +1,13 @@
+"""Main URLs."""
+
+# Django
 from django.urls import path
+
+# Views
 from . import views
 
 app_name = 'main'
-
-urlpatterns = []
+urlpatterns = [
+    path('route', views.route, name='route'),
+    path('map', views.map, name='map')
+]
